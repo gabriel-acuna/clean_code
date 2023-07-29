129b884b0aac7683c251d37db45938261e7405ca
gabriel-acuna <gabriel.acuna.reg27@gmail.com>
2023-07-29 12:44:56 -0500

    Naming variables excersice

---
+++ src/clean_code/01.names.ts
@@ -0,0 +1,51 @@
+(() => {
+
+    // Files to evaluate
+    const fs = [
+        { id: 1, f: false },
+        { id: 2, f: false },
+        { id: 3, f: true },
+        { id: 4, f: false },
+        { id: 5, f: false },
+        { id: 7, f: true },
+    ]; 
+    
+    // Files to delete
+    const arhivos = fs.map( f => f.f );
+
+    
+    class AbstractUser { };
+    class UserMixin { };
+    class UserImplementation { };
+    interface IUser { };
+
+    // Hint
+    class User { };
+    interface User { };
+
+
+    // Todo:
+        
+    // today
+    const ddmmyyyy = new Date();
+    
+    //elapsed time in days
+    const d: number = 23;
+    
+    //number of files in directory
+    const dir = 33;
+    
+    //first name
+    const nombre = 'Gabriel';
+    
+    // last name
+    const apellido = 'Acuña';
+
+    //days since modification
+    const dsm = 12;
+    
+    //max classes per student
+    const max = 6;
+
+
+})();
