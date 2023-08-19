6ffbcfbfcb0300a0fc86e7dacba24a58bc853878
gabriel-acuna <gabriel.acuna.reg27@gmail.com>
2023-08-19 13:19:59 -0500

    DRY Excercise Resolved

--- src/clean_code/05.dry.ts
+++ src/clean_code/05.dry.ts
@@ -1,20 +1,35 @@
 type Size = '' | 'S' | 'M' | 'L'
 class Product {
     constructor(
-       public name: string,
-       public price: number = 0,
-       public size: Size = ''
+        public name: string,
+        public price: number = 0,
+        public size: Size = ''
     ) { }
-    toString(){
-        // No DRY
-        if( this.name.length <= 0) throw Error('name is empty')
-        if( this.price <= 0) throw Error('price is empty')
-        if( this.size.length <= 0) throw Error('size is empty')
+    isAValidProduct(): boolean {
+        let isVaild = true
+        for (const propertyName in this) {
+            switch (typeof this[propertyName]) {
+                case 'string':
+                    if ((<string><unknown>this[propertyName]).length <= 0)
+                        throw new Error(`${propertyName} is empty`)
+                    break;
+                case 'number':
+                    if (<number>(this[propertyName]) <= 0)
+                        throw new Error(`${propertyName} is zero`)
+                    break;
+                default:
+                    throw Error(`Product has not a ${typeof this[propertyName]} property called ${propertyName}`)
+            }
+        }
+        return isVaild;
+    }
+    toString() {
+        if (!this.isAValidProduct()) return
         return `${this.name} ($${this.price}), ${this.size}`
     }
 }
 
 (() => {
-    const bluePants = new Product('Blue Large Pants', 25, 'L');
+    const bluePants = new Product('Blue Large Pants', 30, 'L');
     console.log(bluePants.toString())
 })()
