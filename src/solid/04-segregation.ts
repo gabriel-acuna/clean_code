9b7154dfdcfa46164e154f6e282bba52120dd662
gabriel-acuna <gabriel.acuna.reg27@gmail.com>
2024-07-27 11:15:30 -0500

    ISP practice

---
+++ src/solid/04-segregation.ts
@@ -0,0 +1,32 @@
+interface Bird {
+  fly(): void;
+  eat(): void;
+  run(): void;
+  swimm(): void;
+}
+class Tucan implements Bird {
+  fly(): void {}
+  eat(): void {}
+  run(): void {}
+  swimm(): void {}
+}
+class Humminbird implements Bird {
+  fly(): void {}
+  eat(): void {}
+  run(): void {}
+  swimm(): void {}
+}
+
+class Ostrich implements Bird {
+  fly(): void {}
+  eat(): void {}
+  run(): void {}
+  swimm(): void {}
+}
+
+class Penguin implements Bird {
+  fly(): void {}
+  eat(): void {}
+  run(): void {}
+  swimm(): void {}
+}
